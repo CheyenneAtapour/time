494c0675b99b6c62cb518c38540d60973169b20e
CheyenneAtapour <CheyenneAtapour@gmail.com>
2022-07-03 18:27:44 -0700

    display hundred second format

--- shy_time.py
+++ shy_time.py
@@ -13,4 +13,6 @@ while True:
 	prev_seconds = time.second
 	os.system('cls' if os.name == 'nt' else 'clear')
 	print("{:.2f}".format(seconds / 100) + " / 864")
+	dec_seconds = int(seconds / 100) / 100
+	print("{:.2f}".format(dec_seconds).replace(".", ":")) 
 	#print("\n(Day ends at 864)")
